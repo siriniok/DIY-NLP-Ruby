221221705db74acec6c033b5611f794a40c135f0
Vladyslav Siriniok <siriniok@gmail.com>
2017-11-06 23:28:56 +0200

    Implement loading and preparing word vectors

---
+++ embedding.rb
@@ -0,0 +1,39 @@
+class Embedding
+  def initialize(vocab_file, vectors_file)
+    words = File.open(vocab_file, 'r') do |f|
+      f.readlines.map { |l| l.rstrip.split(' ').first }
+    end
+
+    vectors = {}
+    File.open(vectors_file, 'r') do |f|
+      f.readlines.map do |l|
+        vals = l.rstrip.split(' ')
+        vectors[vals[0]] = vals[1..-1].map(&:to_f)
+      end
+    end
+
+    vocab = Hash[words.each_with_index.map { |w, idx| [w, idx] }]
+    ivocab = Hash[words.each_with_index.map { |w, idx| [idx, w] }]
+
+    vocab_size = words.size
+    vector_dim = vectors[ivocab[0]].size
+
+    w = N.zeros([vocab_size, vector_dim])
+    vectors.map do |word, v|
+      next if word == '<unk>'
+
+      w[vocab[word], 0..-1] = v.clone
+    end
+
+    # normalize each word vector to unit variance
+    w_norm = N.zeros(w.shape)
+    d = ((w**2).sum(1)**(0.5))
+    w_norm = (w.transpose / d).transpose
+
+    @w = w_norm
+    @vocab = vocab
+    @ivocab = ivocab
+  end
+
+  attr_reader :w, :vocab, :ivocab
+end
